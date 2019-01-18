193d2f50275955c0455495837742680f257f2206
weekenwu <fantasy1402@hotmail.com>
2019-01-18 16:36:43 +0800

    Initial commit

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {HeaderWrapper,Logo,NavWrapper,BtnWrapper} from './style';
+
+const Header = () => {
+    return (
+        <HeaderWrapper>
+            <Logo></Logo>
+            <NavWrapper></NavWrapper>
+            <BtnWrapper></BtnWrapper>
+        </HeaderWrapper>
+    )
+};
+
+export default Header;
---
+++ src/components/header/style.js
@@ -0,0 +1,33 @@
+import styled  from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+    width:100%;
+    overflow:hidden;
+`;
+
+export const Logo = styled.div`
+    width:100px;
+    height:58px;
+    float:left;
+    box-sizing:border-box;
+    border:1px solid red;
+`;
+
+export const NavWrapper = styled.div`
+    width:945px;
+    height:58px;
+    float:left;
+    margin-left:40px;
+    box-sizing:border-box;
+    border:1px solid red;
+`;
+
+
+export const BtnWrapper = styled.div`
+    width:200px;
+    height:58px;
+    float:right;
+    margin-right:15px;
+    box-sizing:border-box;
+    border:1px solid red;
+`;
